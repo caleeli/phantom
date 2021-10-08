88e0e95bf25d6d92c67c47eb172a598280e44eeb
David Callizaya <davidcallizaya@gmail.com>
2021-10-08 09:25:39 -0400

    Add amplify

---
+++ src/amplify.js
@@ -0,0 +1,30 @@
+const env = process.env;
+
+aws_amplify.Amplify.configure({
+    aws_project_region: env.cognito_region,
+    aws_cognito_region: env.cognito_region,
+    aws_user_pools_id: env.cognito_pools_id,
+    aws_user_pools_web_client_id: env.cognito_client_id,
+    oauth: {
+        domain: env.cognito_domain,
+        scope: [
+            "phone",
+            "email",
+            "openid",
+            "profile",
+        ],
+        redirectSignIn: `${location.origin}/`,
+        redirectSignOut: `${location.origin}/logout/`,
+        responseType: "token",
+    },
+    federationTarget: "COGNITO_USER_POOLS",
+    aws_cognito_login_mechanisms: ["PREFERRED_USERNAME"],
+    aws_cognito_signup_attributes: ["EMAIL"],
+    aws_cognito_mfa_configuration: "OFF",
+    aws_cognito_mfa_types: ["SMS"],
+    aws_cognito_password_protection_settings: {
+        passwordPolicyMinLength: 8,
+        passwordPolicyCharacters: [],
+    },
+    aws_cognito_verification_mechanisms: ["EMAIL"],
+});
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import App from './App.svelte';
+import './amplify.js';
 
 const app = new App({
 	target: document.body,
