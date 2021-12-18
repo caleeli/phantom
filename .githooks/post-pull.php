d7c64cb34aaa917bbbc567b701a4cbb7f78d4de1
David Callizaya <davidcallizaya@gmail.com>
2021-12-18 19:35:20 -0400

    Set node version

--- .githooks/post-pull.php
+++ .githooks/post-pull.php
@@ -1,7 +1,8 @@
 <?php
+node_version('12');
 run(
     // Update libraries
-    onchange(['composer.json'], 'composer install --no-dev;') .
+    onchange(['composer.json'], 'composer update --no-dev --no-interaction;') .
     // Update libraries
     onchange(['package-lock.json', 'src'], 'npm install;npm run build;') .
     // Application changes
