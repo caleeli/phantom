b8beb04ba23cc012310b0dc86b1ec5fdb56ae124
David Callizaya <davidcallizaya@gmail.com>
2022-04-15 19:59:27 -0400

    Increase default per_page

--- src/components/ABM.svelte
+++ src/components/ABM.svelte
@@ -110,7 +110,7 @@
 	let paramsList = Object.assign(
 		{
 			page: 1,
-			per_page: config["pagination"]?.per_page || 10,
+			per_page: config["pagination"]?.per_page || 50,
 			filter: [],
 			sort: config.sort?.join(","),
 		},
